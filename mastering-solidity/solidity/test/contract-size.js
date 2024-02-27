f850025998d59a044ade95c3dc0c73c62661c9df
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2024-02-27 18:57:30 +0100

    Test Contract Size Limit Summary

---
+++ mastering-solidity/solidity/test/contract-size.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+
+const maxContractSize = 24*1024;
+
+describe("Compiled Smart Contracts Size Test", function () {
+    it("Should deploy the SeedToken contract", async function () {
+        const SeedToken = await ethers.getContractFactory("SeedToken");
+        [deployer] = await ethers.getSigners();
+        const seedToken = await SeedToken.deploy(deployer.address, "Seed Token", "SEED");
+        const deployedAddress = await seedToken.getAddress();
+        expect(deployedAddress).to.exist;
+        const deployedCode = await seedToken.getDeployedCode();
+        //- 2 to remove the leading 0x, /2 because 2 hexadecimal ciphers = 1 byte
+        const size = (deployedCode.length - 2)/2;
+        console.log('    SeedToken contract size: ' + size + ' bytes');
+        expect(size <= maxContractSize).to.be.true;
+    });
+});
