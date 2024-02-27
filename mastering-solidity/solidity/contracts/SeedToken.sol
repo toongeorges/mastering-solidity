94d6740936faa193731a0de04a4eba45c9d20f31
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2024-02-27 19:58:30 +0100

    Hardhat Log Summary

--- mastering-solidity/solidity/contracts/SeedToken.sol
+++ mastering-solidity/solidity/contracts/SeedToken.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.20;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import 'hardhat/console.sol';
 
 error Unauthorized();
 
@@ -22,14 +23,28 @@ contract SeedToken is ERC20 {
     string memory name,
     string memory symbol
   ) ERC20(name, symbol) {
+    console.log(
+      "Creating new cryptocurrency %s (%s) with owner '%s'",
+      name,
+      symbol,
+      ownerAddress
+    );
     owner = ownerAddress;
   }
 
   function changeOwner(address newOwner) external isOwner {
+    console.log(
+      "Changing owner to '%s'",
+      newOwner
+    );
     owner = newOwner;
   }
 
   function mint(uint256 amountOfTokens) external isOwner {
+    console.log(
+      "Minting %s new tokens",
+      amountOfTokens
+    );
     _mint(msg.sender, amountOfTokens * 10**uint(decimals()));
   }
 }
