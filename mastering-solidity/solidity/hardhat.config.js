7bf5a464cbf3e568a5357008983b040374c0e89f
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2024-02-27 20:25:58 +0100

    Configuring Hardhat to Use web3.js Summary

--- mastering-solidity/solidity/hardhat.config.js
+++ mastering-solidity/solidity/hardhat.config.js
@@ -20,3 +20,20 @@ module.exports = {
     }
   }
 };
+
+extendEnvironment((hre) => {
+  const { Web3 } = require("web3");
+  const { Web3Context } = require("web3-core");
+
+  hre.Web3 = Web3;
+  hre.Web3Context = Web3Context;
+
+  // hre.network.provider is an EIP1193-compatible provider.
+  hre.web3 = new Web3(hre.network.provider);
+  hre.web3Context = new Web3Context({
+    provider: hre.network.provider,
+    config: {
+      contractDataInputFill: 'data'
+    } // all new contracts created to populate 'data' field
+  });
+});
