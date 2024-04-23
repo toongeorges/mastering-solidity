9d0d1117e27ed453d51b76a262661e8c9841e55b
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2024-04-23 20:48:32 +0200

    UI: Set Up Material Design Summary

--- mastering-solidity/angular/src/app/app.component.ts
+++ mastering-solidity/angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialDesignModule } from './modules/material-design/material-design.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialDesignModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
