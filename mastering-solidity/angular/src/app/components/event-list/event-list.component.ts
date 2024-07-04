4fed72f43f0208ea04793fd1e162c51f7d8625a7
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2024-07-04 20:04:47 +0200

    Web3: Filter Events on Indexed Parameters Summary

--- mastering-solidity/angular/src/app/components/event-list/event-list.component.ts
+++ mastering-solidity/angular/src/app/components/event-list/event-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MaterialDesignModule } from '../../modules/material-design/material-design.module';
 import { SeedTokenFactoryService } from '../../services/seed-token-factory.service';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-event-list',
@@ -33,13 +34,19 @@ export class EventListComponent {
     if (isNaN(to) || !this.to) {
       to = 'latest';
     }
-    const owner = this.split(this.owner);
+    const owner = this.splitAddress(this.owner);
     const name = this.split(this.name);
     const symbol = this.split(this.symbol);
 
     const contract = this.seedTokenFactoryService.get();
+    const filter = contract.filters.SeedTokenCreation(
+      null,
+      owner,
+      name,
+      symbol
+    );
     const events = await contract.queryFilter(
-      'SeedTokenCreation',
+      filter,
       from,
       to
     );
@@ -58,6 +65,23 @@ export class EventListComponent {
       const split = [];
       values.split(',').forEach((value: string) => {
         split.push(value);
+      });
+      return split;
+    }
+  }
+
+  private splitAddress(values: string): string[] {
+    if (!values) {
+      return null;
+    } else {
+      const split = [];
+      values.split(',').forEach((value: string) => {
+        const address = value.trim();
+        if (ethers.isAddress(address)) {
+          split.push(address);
+        } else {
+          console.error(`'${address}' is not a valid address`);
+        }
       });
       return split;
     }
