49bef06f1b843a54c5eda9e841627a8ba8836ee0
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2024-12-04 19:02:57 +0100

    Types: ConstantVariableDeclaration.sol

---
+++ solidity-in-depth/contracts/10-types/ConstantVariableDeclaration.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+uint constant FIVE = 5; //constant variable declaration
+
+contract ConstantVariableDeclaration {
+    uint constant TEN = 2*FIVE; //state variable declaration
+}
