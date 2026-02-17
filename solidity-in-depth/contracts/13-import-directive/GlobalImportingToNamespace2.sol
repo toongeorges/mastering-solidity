487b746bb4c9d30084cea4ff7321deb9cfe296fb
Toon Macharis <Toon.Macharis+Udemy@gmail.com>
2026-02-17 23:00:50 +0100

    Import Directive: GlobalImportingToNamespace2.sol

---
+++ solidity-in-depth/contracts/13-import-directive/GlobalImportingToNamespace2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.33;
+
+import * as imported from './GlobalImported.sol';
+//this is 100% equivalent to
+//import './GlobalImported.sol' as imported;
+
+contract SimpleContract is imported.A, imported.B { //use contract and interface
+    uint x;
+
+    function simpleFunction() external {
+        imported.C.c();                            //use library
+        imported.d();                              //use global function
+        x = imported.E;                            //use constant value
+        x = imported.F(1).x;                       //use global struct
+        x = uint(imported.G.ONE);                  //use global enum
+        x = imported.H.unwrap(imported.H.wrap(2)); //use global user defined value type
+    }
+
+    function throwError() pure external {
+        revert imported.I();                       //use global error
+    }
+
+    function emitEvent() external {
+        emit imported.J();                         //use global event
+    }
+}
